cb50b45a01dc78e2fa95f50c2e052c130d9a4ef9
Trinigch <trinigch@gmail.com>
2025-08-19 10:31:10 -0500

    -

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import animals from './routes/api/animals_routes'; 
 
 const app = express();
@@ -7,5 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/animals',  animals ); 
+// Servir los archivos estáticos del cliente React
+app.use(express.static(path.join(__dirname, 'client')));
 
+// Ruta catch-all para React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 export default app;
