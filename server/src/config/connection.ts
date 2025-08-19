636d32d5ee17516f5d6ceaf7f0b900e59dd31fc4
Trinigch <trinigch@gmail.com>
2025-08-19 10:17:03 -0500

    ading conexion ssl with render,

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -9,6 +9,9 @@ const pool = new Pool({
   host: process.env.PG_HOST || 'localhost',
   database: process.env.PG_DATABASE,
   port: Number(process.env.PG_PORT) || 5432,
+    ssl: {
+    rejectUnauthorized: false, // permite conexión SSL con Render
+  },
 });
 
 export const getPool = () => pool;
