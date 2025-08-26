72e670e03d23afd8ed6ca3c9f82a792968f86905
Trinigch <trinigch@gmail.com>
2025-08-26 15:25:47 -0500

    cors  especific for render

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,9 +4,14 @@ import path from 'path';
 import animals from './routes/api/animals_routes'; 
 
 const app = express();
-//const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
-
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use('/api/animals',  animals ); 
 // Servir los archivos estáticos del cliente React
