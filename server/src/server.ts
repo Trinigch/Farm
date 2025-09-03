7b940ac9be72209302287e64c6e55f00c6ce692d
Trinigch <trinigch@gmail.com>
2025-09-03 15:22:26 -0500

    morescrtype

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import app from './app';
 import { getPool } from './config/connection';
-
+import { PoolClient } from "pg"; // 👈 importante importar el tipo
 const PORT = process.env.PORT || 3001;
 const pool = getPool();
 
 pool.connect()
-  .then((client) => {
+  .then((client: PoolClient) => {
     client.release();
     console.log('✅ Database connected');
 
@@ -13,7 +13,7 @@ pool.connect()
       console.log(`🚀 Server running on port ${PORT}`);
     });
   })
-  .catch((err) => {
+  .catch((err: Error) => {
     console.error('❌ Error connecting to the database:', err);
   });
   
