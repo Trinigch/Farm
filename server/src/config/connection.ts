316fa9d6a4fe2a167e785c61c9db223bdee2aa45
Trinigch <trinigch@gmail.com>
2025-09-03 12:11:47 -0500

    adding FRONTEND_URL en connections por developer mode

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST || 'localhost',
-  database: process.env.PG_DATABASE,
+  user: process.env.PG_USER || "farm",
+  password: process.env.PG_PASSWORD || "trini",
+  host: process.env.PG_HOST || "localhost",
+  database: process.env.PG_DATABASE || "farm_db",
   port: Number(process.env.PG_PORT) || 5432,
-    ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 export const getPool = () => pool;
